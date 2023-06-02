f7577b4251a53d2cc30e8fa4824cdc54e9f24ffb
VladimirKornev <vladroot@gmail.com>
2023-06-02 18:46:52 +0300

    minor tweaks

--- Assets/Scripts/Navigation.cs
+++ Assets/Scripts/Navigation.cs
@@ -1,8 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obstacle
+public struct Obstacle
 {
     public Collider collider;
     public Transform transform;
